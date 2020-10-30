1b5ce4fb90f542cdbf4050660832e128bd8af2bc
rorodrigofrumento <rodrigo.frumento@gmail.com>
2020-10-30 15:58:40 -0300

    initial

---
+++ src/commons/constants/countries.js
@@ -0,0 +1,35 @@
+import BrazilFlag from 'assets/images/brazil.png'
+import UsaFlag from 'assets/images/usa.png'
+import GermanyFlag from 'assets/images/germany.png'
+import ItalyFlag from 'assets/images/italy.png'
+import ChinaFlag from 'assets/images/china.png'
+
+const Countries = [
+    {
+        label: 'Alemanha',
+        value: 'germany',
+        flag: GermanyFlag
+    },
+    {
+        label: 'Brasil',
+        value: 'brazil',
+        flag: BrazilFlag
+    },
+    {
+        label: 'China',
+        value: 'china',
+        flag: ChinaFlag
+    },
+    {
+        label: 'Itália',
+        value: 'italy',
+        flag: ItalyFlag
+    },
+    {
+        label: 'Estados Unidos',
+        value: 'usa',
+        flag: UsaFlag
+    },
+]
+
+export default Countries
---
+++ src/components/Typography.jsx
@@ -0,0 +1,2 @@
+import Typography from '@material-ui/core/Typography'
+export default Typography
