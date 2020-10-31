74eb59a050fdc8cd0ad1bf39aea4c765d0a33023
rorodrigofrumento <rodrigo.frumento@gmail.com>
2020-10-31 16:09:25 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react'
 import { StylesProvider } from '@material-ui/styles'
 import CssBaseLine  from '@material-ui/core/CssBaseLine'
 import GlobalStyle from '../src/commons/styles/global-style'
+import Main from './containers/Main'
 
 function App() {
   return (
     <StylesProvider injectFirst>
       <CssBaseLine />
       <GlobalStyle />
-      <div>
-        Teste
-      </div>
+      <Main />
     </StylesProvider>
   );
 }
